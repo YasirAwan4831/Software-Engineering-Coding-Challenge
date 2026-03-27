dba7323f09df3c4106b9ff14fce358e661acf539
Muhammad Yasir <my3154831409@gmail.com>
2026-03-28 00:42:59 +0500

    Add Task--4 folder

---
+++ Task--4/assinetask.js
@@ -0,0 +1,33 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+/*
+ * Complete the 'mergeHighDefinitionIntervals' function below.
+ *
+ * The function is expected to return a 2D_INTEGER_ARRAY.
+ * The function accepts 2D_INTEGER_ARRAY intervals as parameter.
+ */
+
+function mergeHighDefinitionIntervals(intervals) {
+}
